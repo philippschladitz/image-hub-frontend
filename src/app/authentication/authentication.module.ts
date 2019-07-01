4b762474ed6193a915dfd970ef88f3283456f4f6
julvmdt <juliavamboldt@gmx.de>
2019-07-01 21:32:47 +0200

    connect to backend, make registration and login possible and implement snackbar

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthenticationRoutingModule } from './authentication-routing.module';
-import { RegistrationComponent } from './registration/registration.component';
-import { AuthenticationComponent } from './authentication/authentication.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { RegistrationComponent } from './registration/registration.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
 import { AuthenticationCardComponent } from './authentication-card/authentication-card.component';
 import { LoginComponent } from './login/login.component';
 
@@ -27,6 +29,7 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatButtonModule,
     MatInputModule,
+    MatSnackBarModule,
     FormsModule,
     ReactiveFormsModule
   ]
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +10,38 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly authenticationService: AuthenticationService,
+    private readonly matSnackBar: MatSnackBar
+  ) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: this.formBuilder.control('', [
+        Validators.required, 
+        Validators.email
+      ]),
+      password: this.formBuilder.control('', [Validators.required])
+    });
   }
 
   login() {
-    console.log('Login');
+    if (this.form.valid) {
+      this.authenticationService.login({
+        email: this.form.value.email,
+        password: this.form.value.password
+      }).subscribe(loginResponse => {
+        if (loginResponse.status === 401) {
+          this.matSnackBar.open('E-Mail oder Passwort falsch.', null);
+        }
+      }, err => {
+        this.matSnackBar.open('Der Server ist nicht erreichbar.', null, { duration: 2000 });
+      });
+    } else {
+      this.matSnackBar.open('Bitte geben Sie E-Mail und Passwort ein.', null, { duration: 2000 });
+    }
   }
 }
